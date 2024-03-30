ca25e24e7c9ad9f46ee9f815c455196980c201d0
Assis <assisneto013@gmail.com>
2024-03-30 18:45:31 -0400

    Correção da lógica dos cards e criação dos mesmos

---
+++ src/components/ActionButtons.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <q-btn icon="edit" color="info" dense size="sm" @click="editClick" v-if="editVisible" />
+    <q-btn icon="delete" color="negative" dense size="sm" @click="deleteClick" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    editVisible: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    editClick() {
+      this.$emit('edit-click');
+    },
+    deleteClick() {
+      this.$emit('delete-click');
+    }
+  }
+};
+</script>
